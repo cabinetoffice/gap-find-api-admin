4d497297067ff4f86fcf71936255932642111cee
rachelswart <rachel.swart@cabinetoffice.gov.uk>
2023-08-09 14:12:00 +0100

    initial commit

---
+++ src/test/java/gov/caninetoffice/gapfindapiadmin/GapFindApiAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.caninetoffice.gapfindapiadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GapFindApiAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
