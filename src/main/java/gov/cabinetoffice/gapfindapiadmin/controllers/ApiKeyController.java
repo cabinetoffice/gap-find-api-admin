0537cd100732531bcb5f34bd05b1a720c7af13ac
Connor Macqueen <138442814+ConnorTCO@users.noreply.github.com>
2023-08-15 15:12:11 +0100

    Adding controller test

--- src/main/java/gov/cabinetoffice/gapfindapiadmin/controllers/ApiKeyController.java
+++ src/main/java/gov/cabinetoffice/gapfindapiadmin/controllers/ApiKeyController.java
@@ -3,9 +3,9 @@ package gov.cabinetoffice.gapfindapiadmin.controllers;
 import gov.cabinetoffice.gapfindapiadmin.service.ApiKeyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/api-keys")
@@ -15,8 +15,9 @@ public class ApiKeyController {
     private final ApiKeyService apiKeyService;
 
     @GetMapping
-    public String showKeys(Model model) {
-        model.addAttribute("apiKeys", apiKeyService.getApiKeysForFundingOrganisation(2));
-        return "organisation-api-keys";
+    public ModelAndView showKeys() {
+        ModelAndView mav = new ModelAndView("organisation-api-keys");
+        mav.addObject("apiKeys", apiKeyService.getApiKeysForFundingOrganisation(2));
+        return mav;
     }
 }
---
+++ src/test/java/gov/cabinetoffice/gapfindapiadmin/controllers/ApiKeyControllerTest.java
@@ -0,0 +1,57 @@
+package gov.cabinetoffice.gapfindapiadmin.controllers;
+
+import gov.cabinetoffice.gapfindapiadmin.model.ApiKey;
+import gov.cabinetoffice.gapfindapiadmin.service.ApiKeyService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ApiKeyControllerTest {
+
+    @Mock
+    private ApiKeyService apiKeyService;
+
+    @InjectMocks
+    private ApiKeyController controllerUnderTest;
+
+    @Test
+    public void showKeys_expectedResponse(){
+
+        final String apiKey = "Key";
+        final List<ApiKey> expectedApiKeys = List.of(ApiKey.builder().apiKey(apiKey).build());
+
+        when(apiKeyService.getApiKeysForFundingOrganisation(any(Integer.class))).thenReturn(expectedApiKeys);
+
+        ModelAndView actualResponse = controllerUnderTest.showKeys();
+
+
+        List<ApiKey> actualApiKeys = (List<ApiKey>) actualResponse.getModel().get("apiKeys");
+
+        assertThat(actualResponse.getModel().get("apiKeys")).isEqualTo(expectedApiKeys);
+        assertThat(actualApiKeys.get(0).getApiKey()).isEqualTo(apiKey);
+    }
+
+    @Test
+    public void showKeys_expectedResponse_emptyList(){
+
+        List<ApiKey> expectedApiKeys = new ArrayList<>();
+
+        when(apiKeyService.getApiKeysForFundingOrganisation(any(Integer.class))).thenReturn(expectedApiKeys);
+
+        ModelAndView actualResponse = controllerUnderTest.showKeys();
+        assertThat(actualResponse.getModel().get("apiKeys")).isEqualTo(expectedApiKeys);
+
+    }
+
+}
