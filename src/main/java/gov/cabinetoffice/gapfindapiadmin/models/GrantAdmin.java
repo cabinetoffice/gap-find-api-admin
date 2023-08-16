d99e23b6ced6bbc05e743f5cacb9393ac3c15ece
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2023-08-16 15:21:40 +0100

    remo

--- src/main/java/gov/cabinetoffice/gapfindapiadmin/models/GrantAdmin.java
+++ src/main/java/gov/cabinetoffice/gapfindapiadmin/models/GrantAdmin.java
@@ -17,10 +17,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
 
 
 @Entity
