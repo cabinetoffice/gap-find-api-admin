c8755b8b9ae3baf3c0ec59002ad64a8176899e5c
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2023-08-16 14:53:15 +0100

    remove unused field

--- src/main/java/gov/cabinetoffice/gapfindapiadmin/models/GrantAdmin.java
+++ src/main/java/gov/cabinetoffice/gapfindapiadmin/models/GrantAdmin.java
@@ -31,7 +31,7 @@ import java.util.Collection;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class GrantAdmin implements UserDetails {
+public class GrantAdmin {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -47,38 +47,4 @@ public class GrantAdmin implements UserDetails {
     @JoinColumn(name = "user_id", referencedColumnName = "gap_user_id")
     private GapUser gapUser;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 }
