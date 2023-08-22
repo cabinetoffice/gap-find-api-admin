6ea85b4214db1ecd47d1b3cc8135b809c4dd8df2
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2023-08-22 17:08:20 +0100

    add RBAC on views. making sure that the superAdming or TechnicalSupport will only access their respective pages. Adds exception handling at filter level

--- src/main/java/gov/cabinetoffice/gapfindapiadmin/controllers/ApiKeyController.java
+++ src/main/java/gov/cabinetoffice/gapfindapiadmin/controllers/ApiKeyController.java
@@ -7,11 +7,17 @@ import gov.cabinetoffice.gapfindapiadmin.services.ApiGatewayService;
 import gov.cabinetoffice.gapfindapiadmin.services.ApiKeyService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,11 +29,15 @@ public class ApiKeyController {
     public static final String ORGANISATION_API_KEYS_PAGE = "organisation-api-keys";
     public static final String REVOKE_API_KEY_CONFIRMATION_PAGE = "revoke-api-key-confirmation";
     public static final String ERROR_PAGE = "error-page";
+    public static final String SUPER_ADMIN_PAGE = "super";
+    public final static String SUPER_ADMIN_ROLE = "SUPER_ADMIN";
+    public final static String TECHNICAL_SUPPORT_ROLE = "TECHNICAL_SUPPORT";
 
     private final ApiKeyService apiKeyService;
     private final ApiGatewayService apiGatewayService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('TECHNICAL_SUPPORT')")
     public ModelAndView showKeys() {
         final GrantAdmin grantAdmin = (GrantAdmin) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         final String departmentName = grantAdmin.getFunder().getName();
@@ -37,13 +47,15 @@ public class ApiKeyController {
                 .addObject("departmentName", departmentName);
     }
 
+    @PreAuthorize("hasAuthority('TECHNICAL_SUPPORT')")
     @GetMapping("/create")
     public ModelAndView showCreateKeyForm() {
         final ModelAndView createApiKey = new ModelAndView(CREATE_API_KEY_FORM_PAGE);
         return createApiKey.addObject("createApiKeyDTO", new CreateApiKeyDTO());
     }
 
-    @PostMapping("/create")
+    @PreAuthorize("hasAuthority('TECHNICAL_SUPPORT')")
+    @Secured(TECHNICAL_SUPPORT_ROLE)
     public ModelAndView createKey(final @Valid @ModelAttribute CreateApiKeyDTO createApiKeyDTO, final BindingResult bindingResult) {
         if (apiKeyService.doesApiKeyExist(createApiKeyDTO.getKeyName())) {
             final FieldError duplicateKey = new FieldError("createApiKeyDTO",
@@ -60,27 +72,35 @@ public class ApiKeyController {
                     .addObject("createApiKeyDTO", createApiKeyDTO);
         }
 
-        return  new ModelAndView(NEW_API_KEY_PAGE)
+        return new ModelAndView(NEW_API_KEY_PAGE)
                 .addObject("keyValue", apiGatewayService.createApiKeysInAwsAndDb(createApiKeyDTO.getKeyName()));
     }
 
     @GetMapping("/revoke/{apiKeyId}")
+    @PreAuthorize("hasAuthority('TECHNICAL_SUPPORT') || hasAuthority('SUPER_ADMIN')")
     public ModelAndView showRevokeApiKeyConfirmation(@PathVariable int apiKeyId) {
         final GapApiKey apiKey = apiKeyService.getApiKeyById(apiKeyId);
         return new ModelAndView(REVOKE_API_KEY_CONFIRMATION_PAGE)
-                .addObject("apiKey", apiKey);
+                .addObject("apiKey", apiKey).addObject("backButtonUrl", apiKeyService.generateBackButtonValue());
     }
 
     @PostMapping("/revoke")
+    @PreAuthorize("hasAuthority('TECHNICAL_SUPPORT') || hasAuthority('SUPER_ADMIN')")
     public String revokeApiKey(@ModelAttribute GapApiKey apiKey) {
         // TODO: see if we can do this in one transaction
         apiGatewayService.deleteApiKey(apiKeyService.getApiKeyById(apiKey.getId()));
         apiKeyService.revokeApiKey(apiKey.getId());
-        return "redirect:/api-keys";
+        return "redirect:" + apiKeyService.generateBackButtonValue();
     }
 
     @GetMapping("/error")
     public ModelAndView displayError() {
-        return new ModelAndView(ERROR_PAGE);
+        return new ModelAndView(ERROR_PAGE).addObject("backButtonUrl", apiKeyService.generateBackButtonValue());
+    }
+
+    @GetMapping("/super-admin")
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
+    public ModelAndView displaySuperAdminPage() {
+        return new ModelAndView(SUPER_ADMIN_PAGE);
     }
 }
