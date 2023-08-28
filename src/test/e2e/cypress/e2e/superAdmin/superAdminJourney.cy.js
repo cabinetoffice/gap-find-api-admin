32504d845d28230dad4fe898f54391024b525c7c
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2023-08-28 10:09:43 +0100

    change test to, first check 10 keys

--- src/test/e2e/cypress/e2e/superAdmin/superAdminJourney.cy.js
+++ src/test/e2e/cypress/e2e/superAdmin/superAdminJourney.cy.js
@@ -18,11 +18,11 @@ describe('Super Admin Journey', () => {
   // });
 
   // it('creates keys for superAdmin test', () => {
-  //   create55KeyForFUndingOrganisation1();
-  //   create55KeyForFUndingOrganisation2();
+  //   createKeyForFUndingOrganisation1(0, 5);
+  //   createKeyForFUndingOrganisation2(5, 10);
   // });
 
-  it('should show 110 keys and 2 organisation in the filters', () => {
+  it('should show 10 keys and 2 organisation in the filters', () => {
     cy.setMockTokenForSuperAdmin();
     cy.visit('http://localhost:8084/find/api/admin/api-keys/manage');
 
@@ -30,7 +30,7 @@ describe('Super Admin Journey', () => {
     cy.get(`[data-cy="admin-dashboard-heading"]`).should('be.visible').should('have.text', 'Manage API keys');
     cy.get(`[data-cy="admin-dashboard-active-key-count"]`)
       .should('be.visible')
-      .should('have.text', '110 active API keys');
+      .should('have.text', '10 active API keys');
     cy.get('[data-cy="admin-dashboard-filter-departments-div"]')
       .should('be.visible')
       .find('input[type="checkbox"]')
@@ -67,30 +67,35 @@ describe('Super Admin Journey', () => {
 function checkFirst10RowsContent(today) {
   for (let i = 0; i < 10; i++) {
     const index = i + 1;
-    cy.get(`[data-cy="admin-dashboard-list-table-row-API-key-Org1Cypress${index}"]`).should(
+    const keyName = index <= 5 ? 'Org1Cypress' : 'Org2Cypress';
+    const organisationName = index <= 5 ? 'Test Org' : 'Evil Org';
+    cy.get(`[data-cy="admin-dashboard-list-table-row-API-key-${keyName + index}"]`).should(
       'have.text',
-      `Org1Cypress${index}`
+      `${keyName + index}`
     );
-    cy.get(`[data-cy="admin-dashboard-list-table-row-Department-Org1Cypress${index}"]`).should('have.text', 'Test Org');
-    cy.get(`[data-cy="admin-dashboard-list-table-row-Created-Org1Cypress${index}"]`).should('have.text', today);
-    cy.get(`[data-cy="admin-dashboard-list-table-row-Revoked-Org1Cypress${index}-link"]`).should('contain', 'Revoke');
+    cy.get(`[data-cy="admin-dashboard-list-table-row-Department-${keyName + index}"]`).should(
+      'have.text',
+      organisationName
+    );
+    cy.get(`[data-cy="admin-dashboard-list-table-row-Created-${keyName + index}"]`).should('have.text', today);
+    cy.get(`[data-cy="admin-dashboard-list-table-row-Revoked-${keyName + index}-link"]`).should('contain', 'Revoke');
   }
 }
-// function create55KeyForFUndingOrganisation1() {
-//   cy.setMockTokenForTechnicalSupportFundingOrganisation1();
-//   for (let i = 0; i < 55; i++) {
-//     cy.visit('http://localhost:8084/find/api/admin/api-keys/create');
-//     cy.get(`[data-cy="create-key-input"]`).type('Org1Cypress' + (i + 1));
-//     cy.get(`[data-cy="create-key-continue"]`).click();
-//     cy.get(`[data-cy="new-key-back-button"]`).click();
-//   }
-// }
-// function create55KeyForFUndingOrganisation2() {
-//   cy.setMockTokenForTechnicalSupportFundingOrganisation2();
-//   for (let i = 55; i < 110; i++) {
-//     cy.visit('http://localhost:8084/find/api/admin/api-keys/create');
-//     cy.get(`[data-cy="create-key-input"]`).type('Org2Cypress' + (i + 1));
-//     cy.get(`[data-cy="create-key-continue"]`).click();
-//     cy.get(`[data-cy="new-key-back-button"]`).click();
-//   }
-// }
+function createKeyForFUndingOrganisation1(startingPoint, endingPoint) {
+  cy.setMockTokenForTechnicalSupportFundingOrganisation1();
+  for (let i = startingPoint; i < endingPoint; i++) {
+    cy.visit('http://localhost:8084/find/api/admin/api-keys/create');
+    cy.get(`[data-cy="create-key-input"]`).type('Org1Cypress' + (i + 1));
+    cy.get(`[data-cy="create-key-continue"]`).click();
+    cy.get(`[data-cy="new-key-back-button"]`).click();
+  }
+}
+function createKeyForFUndingOrganisation2(startingPoint, endingPoint) {
+  cy.setMockTokenForTechnicalSupportFundingOrganisation2();
+  for (let i = startingPoint; i < endingPoint; i++) {
+    cy.visit('http://localhost:8084/find/api/admin/api-keys/create');
+    cy.get(`[data-cy="create-key-input"]`).type('Org2Cypress' + (i + 1));
+    cy.get(`[data-cy="create-key-continue"]`).click();
+    cy.get(`[data-cy="new-key-back-button"]`).click();
+  }
+}
