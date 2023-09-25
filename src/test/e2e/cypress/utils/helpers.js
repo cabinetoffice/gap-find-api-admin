1cd9af563f526515db94dfcd9e3278420efb5789
a-lor-cab <107372333+a-lor-cab@users.noreply.github.com>
2023-09-25 15:02:43 +0100

    Tmi2 249/add nav bar link to swagger (#34)
    * add apiDocumentation Link to technical support views. need tests
    
    * add unit test
    
    * add cypress coverage, fix typo and add padding to navbar item

--- src/test/e2e/cypress/utils/helpers.js
+++ src/test/e2e/cypress/utils/helpers.js
@@ -1,10 +1,15 @@
 function checkNavBarItemIsRightForTheUserRole(userRole) {
-  const href =
-    userRole === 'SUPER_ADMIN'
-      ? 'http://localhost:3000/apply/admin/super-admin-dashboard'
-      : 'http://localhost:3000/apply/admin/dashboard';
-  const displayName = userRole === 'SUPER_ADMIN' ? 'Super Admin Dashboard' : 'Admin Dashboard';
+  const isSuperAdmin = userRole === 'SUPER_ADMIN';
+  const href = isSuperAdmin
+    ? 'http://localhost:3000/apply/admin/super-admin-dashboard'
+    : 'http://localhost:3000/apply/admin/dashboard';
+  const displayName = isSuperAdmin ? 'Super admin dashboard' : 'Admin dashboard';
   cy.get('[data-cy="header-navbar-back-to-dashboard-link"]').should('have.attr', 'href', href).contains(displayName);
+  if (!isSuperAdmin) {
+    cy.get('[data-cy="header-navbar-api-documentation-link"]')
+      .should('have.attr', 'href', 'http://localhost:8088/apply/open-data/swagger-ui/index.html')
+      .contains('Api documentation');
+  }
 }
 
 function signOutIsPresent() {
