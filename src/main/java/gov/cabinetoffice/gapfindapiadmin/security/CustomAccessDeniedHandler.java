6e85c6fec896e1882d1bd430ca8918530ca28755
GavCookCO <99668051+GavCookCO@users.noreply.github.com>
2024-01-19 11:54:33 +0000

    TMI2-536: Automated test fixes - improving error state journey (#48)
    * improving error state journey for unauthenticated users
    
    * removing pipeline actions to run integration tests
    
    * updating aws credentials provider

--- src/main/java/gov/cabinetoffice/gapfindapiadmin/security/CustomAccessDeniedHandler.java
+++ src/main/java/gov/cabinetoffice/gapfindapiadmin/security/CustomAccessDeniedHandler.java
@@ -2,6 +2,7 @@ package gov.cabinetoffice.gapfindapiadmin.security;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -9,10 +10,12 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 // this class handles unsuccessful authentication attempts
+@Slf4j
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException {
+        log.info("In CustomAccessDeniedHandler");
         httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/api-keys/error");
     }
 }
