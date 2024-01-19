e98f03a18802d8f2009b869eade68180cf2bf4fb
a-lor-cab <107372333+a-lor-cab@users.noreply.github.com>
2024-01-19 10:21:27 +0000

    add logs (#47)

--- src/main/java/gov/cabinetoffice/gapfindapiadmin/services/ApiGatewayService.java
+++ src/main/java/gov/cabinetoffice/gapfindapiadmin/services/ApiGatewayService.java
@@ -6,10 +6,14 @@ import gov.cabinetoffice.gapfindapiadmin.exceptions.UnauthorizedException;
 import gov.cabinetoffice.gapfindapiadmin.models.GapApiKey;
 import gov.cabinetoffice.gapfindapiadmin.models.GrantAdmin;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient;
-import software.amazon.awssdk.services.apigateway.model.*;
+import software.amazon.awssdk.services.apigateway.model.CreateApiKeyRequest;
+import software.amazon.awssdk.services.apigateway.model.CreateApiKeyResponse;
+import software.amazon.awssdk.services.apigateway.model.CreateUsagePlanKeyRequest;
+import software.amazon.awssdk.services.apigateway.model.DeleteApiKeyRequest;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -20,6 +24,7 @@ import static java.util.Optional.ofNullable;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ApiGatewayService {
 
     private final ApiGatewayConfigProperties apiGatewayConfigProperties;
@@ -30,16 +35,23 @@ public class ApiGatewayService {
 
     public String createApiKeysInAwsAndDb(String keyName) {
         final GrantAdmin grantAdmin = (GrantAdmin) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
         //create the api key in aws api gateway
         final CreateApiKeyResponse apiKey = createApiKeyInAwsApiGateway(keyName);
+        log.info("Api key created in apiGateway");
+
         // set the usage plan for the created api key.
         addApiKeyToApiGatewayUsagePlan(apiKey);
+
         // save the api key in the database
         saveKeyInDatabase(keyName, apiKey, grantAdmin);
+
         return apiKey.value();
     }
 
     protected CreateApiKeyResponse createApiKeyInAwsApiGateway(String keyName) {
+        log.info("Creating api key in api gateway with name: {}", keyName);
+
         final CreateApiKeyRequest apiKeyRequest = CreateApiKeyRequest.builder()
                 .name(keyName)
                 .enabled(true)
@@ -50,6 +62,8 @@ public class ApiGatewayService {
     }
 
     protected void addApiKeyToApiGatewayUsagePlan(CreateApiKeyResponse apiKey) {
+        log.info("Adding api key to usage plan");
+
         final CreateUsagePlanKeyRequest planRequest = CreateUsagePlanKeyRequest.builder()
                 .usagePlanId(apiGatewayConfigProperties.getApiGatewayUsagePlanId())
                 .keyId(apiKey.id())
@@ -57,10 +71,13 @@ public class ApiGatewayService {
                 .build();
 
         apiGatewayClient.createUsagePlanKey(planRequest);
+
+        log.info("Api key added to usage plan");
     }
 
     protected void saveKeyInDatabase(String keyName, CreateApiKeyResponse response, GrantAdmin grantAdmin) {
         //TODO add in some error handling here in case it doesn't save to the db for some reason but we manage to save in aws? Could catch any db exceptions and then delete from aws if it exists.
+        log.info("Saving api key in database");
 
         try {
             final GapApiKey apiKey = GapApiKey.builder()
@@ -73,6 +90,9 @@ public class ApiGatewayService {
                     .build();
 
             apiKeyService.saveApiKey(apiKey);
+
+            log.info("Api key saved in database");
+
         } catch (NoSuchAlgorithmException e) {
             throw new ApiKeyException(e);
         }
@@ -80,6 +100,8 @@ public class ApiGatewayService {
     }
 
     private String hashString(String hashingValue) throws NoSuchAlgorithmException {
+        log.info("Hashing api key");
+
         final MessageDigest md = MessageDigest.getInstance("SHA-512");
         md.update(hashingValue.getBytes(StandardCharsets.UTF_8));
         final byte[] hashed = md.digest();
@@ -95,12 +117,18 @@ public class ApiGatewayService {
     }
 
     public void deleteApiKey(GapApiKey apiKey, boolean isSuperAdmin) {
+        log.info("deleting Api key from api gateway with id: {}", apiKey.getApiGatewayId());
+
         final GrantAdmin grantAdmin = (GrantAdmin) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         ofNullable(grantAdmin).filter(admin -> admin.getFunder().getName().equals(apiKey.getFundingOrganisation().getName()) || isSuperAdmin)
                 .ifPresentOrElse(
-                        admin -> apiGatewayClient.deleteApiKey(DeleteApiKeyRequest.builder()
-                                .apiKey(apiKey.getApiGatewayId())
-                                .build()),
+                        admin -> {
+                            apiGatewayClient.deleteApiKey(DeleteApiKeyRequest.builder()
+                                    .apiKey(apiKey.getApiGatewayId())
+                                    .build());
+
+                            log.info("Api key deleted from api gateway");
+                        },
                         () -> {
                             throw new UnauthorizedException("User is unauthorized to revoke this API key");
                         }
