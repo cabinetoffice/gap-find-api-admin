e93db6f77e8ec901a787ddfe1d10f233749446a5
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2024-03-11 12:26:51 +0000

    fix message

--- src/main/java/gov/cabinetoffice/gapfindapiadmin/dtos/CreateApiKeyDTO.java
+++ src/main/java/gov/cabinetoffice/gapfindapiadmin/dtos/CreateApiKeyDTO.java
@@ -17,9 +17,9 @@ import lombok.NoArgsConstructor;
 public class CreateApiKeyDTO {
 
 	@NotBlank(message = "Enter a key name")
-	@Pattern(regexp = "^(?!\\s).*", message = "Key name must not start with empty spaces")
+	@Pattern(regexp = "^(?!\\s).*", message = "Key name must not start with empty space")
 	@Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "Key name must be alphanumeric")
-	@Pattern(regexp = "^(.*\\S)?$", message = "Key name  must not end with a space")
+	@Pattern(regexp = "^(.*\\S)?$", message = "Key name  must not end with empty space")
 	@Size(max = 1024, message = "Key name must be max 1024 characters")
 	private String keyName;
 
